ca9b2ea02cebaedce1f676f1828b259809bc13f4
William Rågstad <41281398+WilliamRagstad@users.noreply.github.com>
2022-02-08 20:21:28 +0100

    Refactor IController

--- src/IController.ts
+++ src/IController.ts
@@ -1,43 +1,43 @@
-import { Context } from "https://deno.land/x/oak/mod.ts";
+import { Context } from "./types.ts";
 
 export default abstract class IController {
-  path = "/";
+	path = "/";
 
-  /**
-   * METHOD GET /
-   * @description Fetch all instances of the relevant model
-   * @param ctx Oak request context
-   */
-  get?(ctx: Context): void | Promise<void>;
+	/**
+	 * METHOD GET /
+	 * @description Fetch all instances of the relevant model
+	 * @param ctx Oak request context
+	 */
+	get?(ctx: Context): void | Promise<void>;
 
-  /**
-   * METHOD GET /:id
-   * @description Fetch a single instance of the relevant model
-   * @param id Id of the resource
-   * @param ctx Oak request context
-   */
-  getById?(id: string, ctx: Context): void | Promise<void>;
+	/**
+	 * METHOD GET /:id
+	 * @description Fetch a single instance of the relevant model
+	 * @param id Id of the resource
+	 * @param ctx Oak request context
+	 */
+	getById?(id: string, ctx: Context): void | Promise<void>;
 
-  /**
-   * METHOD POST /
-   * @description Create a new instance of the relevant model
-   * @param ctx Oak request context
-   */
-  post?(ctx: Context): void | Promise<void>;
+	/**
+	 * METHOD POST /
+	 * @description Create a new instance of the relevant model
+	 * @param ctx Oak request context
+	 */
+	post?(ctx: Context): void | Promise<void>;
 
-  /**
-   * METHOD DELETE /:id
-   * @description Delete a single instance of the relevant model
-   * @param id Id of the resource
-   * @param ctx Oak request context
-   */
-  delete?(id: string, ctx: Context): void | Promise<void>;
+	/**
+	 * METHOD DELETE /:id
+	 * @description Delete a single instance of the relevant model
+	 * @param id Id of the resource
+	 * @param ctx Oak request context
+	 */
+	delete?(id: string, ctx: Context): void | Promise<void>;
 
-  /**
-   * METHOD PUT /:id
-   * @description Update a single instance of the relevant model
-   * @param id Id of the resource
-   * @param ctx Oak request context
-   */
-  put?(id: string, ctx: Context): void | Promise<void>;
+	/**
+	 * METHOD PUT /:id
+	 * @description Update a single instance of the relevant model
+	 * @param id Id of the resource
+	 * @param ctx Oak request context
+	 */
+	put?(id: string, ctx: Context): void | Promise<void>;
 }
