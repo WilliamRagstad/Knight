47adba92c6eb525ebe350133f3b4e1989ddbf47b
William Rågstad <41281398+WilliamRagstad@users.noreply.github.com>
2022-02-08 20:21:18 +0100

    Export everything from types

--- src/types.ts
+++ src/types.ts
@@ -1,29 +1,57 @@
 // deno-lint-ignore-file no-explicit-any
 import {
-  RouteParams,
-  RouterContext,
-  RouterMiddleware,
-  State,
+	Application,
+	Router,
+	RouteParams,
+	RouterContext,
+	RouterMiddleware,
+	Request,
+	Response,
+	Context,
+	HTTPMethods,
+	State
 } from "https://deno.land/x/oak/mod.ts";
+import IController from "./IController.ts";
 
 export type Constructor<T> = new (...args: any[]) => T;
 
 export type Params = RouteParams<string>;
 
 export type StringRouterMiddleware = RouterMiddleware<
-  string,
-  Params,
-  Record<string, any>
+	string,
+	Params,
+	Record<string, any>
 >;
 export type StringRouterContext = RouterContext<
-  string,
-  Params,
-  Record<string, any>
+	string,
+	Params,
+	Record<string, any>
 >;
 
 export type UnknownReturn = Promise<unknown> | unknown | Promise<void> | void;
 
 export type StringEndpoint = (
-  params: Params,
-  ctx: RouterContext<string, Params, Record<string, any>>,
+	params: Params,
+	ctx: RouterContext<string, Params, Record<string, any>>,
 ) => UnknownReturn;
+
+
+export enum AppMode {
+	DEV,
+	PROD,
+}
+
+export {
+	IController,
+	Application,
+	Router, Request,
+	Response,
+	Context
+};
+export type {
+	HTTPMethods,
+	State,
+	RouterContext,
+	RouteParams,
+	RouterMiddleware
+};
