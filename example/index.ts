a62c029c55ccf31249541c6b25198efbac065a82
William Rågstad <41281398+WilliamRagstad@users.noreply.github.com>
2022-02-25 14:44:36 +0100

    Use new build method

--- example/index.ts
+++ example/index.ts
@@ -1,10 +1,5 @@
 import { Knight } from "../mod.ts";
-import UserController from "./controller/UserController.ts";
 
-const app = Knight.createApi([
-  new UserController(),
-]);
-
+const app = await Knight.build();
 console.log("Server ready on http://localhost:8000");
-
 await app.listen({ port: 8000 });
