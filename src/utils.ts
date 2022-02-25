5951ce0960b9dfab4cc3377d817749a977c0c916
William Rågstad <41281398+WilliamRagstad@users.noreply.github.com>
2022-02-25 13:22:59 +0100

    Format

--- src/utils.ts
+++ src/utils.ts
@@ -13,80 +13,80 @@ import { Constructor, Request, Response } from "./types.ts";
  * @returns The mapped DTO
  */
 export async function bodyMappingJSON<TModel>(
-	request: Request,
-	dtoType: Constructor<TModel>,
+  request: Request,
+  dtoType: Constructor<TModel>,
 ): Promise<TModel> {
-	const parsed = await request.body({ type: "json" }).value;
-	return mapping(parsed, dtoType);
+  const parsed = await request.body({ type: "json" }).value;
+  return mapping(parsed, dtoType);
 }
 export async function bodyMappingForm<TModel>(
-	request: Request,
-	dtoType: Constructor<TModel>,
+  request: Request,
+  dtoType: Constructor<TModel>,
 ): Promise<TModel> {
-	const parsed = await request.body({ type: "form" }).value;
-	return mapping(paramsToObject(parsed), dtoType);
+  const parsed = await request.body({ type: "form" }).value;
+  return mapping(paramsToObject(parsed), dtoType);
 }
 export async function bodyMappingFormData<TModel>(
-	request: Request,
-	dtoType: Constructor<TModel>,
+  request: Request,
+  dtoType: Constructor<TModel>,
 ): Promise<TModel> {
-	const parsed = await request.body({ type: "form-data" }).value.read();
-	return mapping(parsed.fields, dtoType);
+  const parsed = await request.body({ type: "form-data" }).value.read();
+  return mapping(parsed.fields, dtoType);
 }
 function paramsToObject(entries: URLSearchParams) {
-	const result: Record<string, any> = {}
-	for (const [key, value] of entries) {
-		result[key] = value;
-	}
-	return result;
+  const result: Record<string, any> = {};
+  for (const [key, value] of entries) {
+    result[key] = value;
+  }
+  return result;
 }
 
 function mapping<T>(data: Record<string, any>, type: Constructor<T>): T {
-	const diff = difference(data, type);
-	if (!diff.matching) {
-		throw new Error(
-			"Invalid request body, expected " + type.name + " DTO." +
-			(diff.missing.length > 0
-				? ` Missing fields: ${diff.missing.join(", ")}.`
-				: "") +
-			(diff.invalid.length > 0
-				? ` Invalid fields: ${diff.invalid.join(", ")}.`
-				: ""),
-		);
-	}
-	return data as T;
+  const diff = difference(data, type);
+  if (!diff.matching) {
+    throw new Error(
+      "Invalid request body, expected " + type.name + " DTO." +
+        (diff.missing.length > 0
+          ? ` Missing fields: ${diff.missing.join(", ")}.`
+          : "") +
+        (diff.invalid.length > 0
+          ? ` Invalid fields: ${diff.invalid.join(", ")}.`
+          : ""),
+    );
+  }
+  return data as T;
 }
 
 function difference<TModel>(
-	json: Record<string, any>,
-	dtoType: Constructor<TModel>,
+  json: Record<string, any>,
+  dtoType: Constructor<TModel>,
 ): { matching: boolean; missing: string[]; invalid: string[] } {
-	const jsonKeys = Object.keys(json);
-	const dtoFields = classFields(dtoType);
-	const missing = dtoFields.filter((field) =>
-		!jsonKeys.includes(field.name) && !field.optional
-	).map((field) => field.name);
-	const invalid = jsonKeys.filter((key) =>
-		!dtoFields.find((field) => field.name === key)
-	);
-	return {
-		matching: missing.length === 0 && invalid.length === 0,
-		missing,
-		invalid,
-	};
+  const jsonKeys = Object.keys(json);
+  const dtoFields = classFields(dtoType);
+  const missing = dtoFields.filter((field) =>
+    !jsonKeys.includes(field.name) && !field.optional
+  ).map((field) => field.name);
+  const invalid = jsonKeys.filter((key) =>
+    !dtoFields.find((field) => field.name === key)
+  );
+  return {
+    matching: missing.length === 0 && invalid.length === 0,
+    missing,
+    invalid,
+  };
 }
 
 function classFields<T>(
-	classType: Constructor<T>,
+  classType: Constructor<T>,
 ): { name: string; optional: boolean }[] {
-	const t = classType.toString();
-	const optionals: string[] = classType.prototype.optionals ?? [];
-	const fields = t.substring(t.indexOf("{") + 1, t.indexOf("constructor"))
-		.split(";").map((s) => s.trim()).filter((s) => s.length > 0).map((f) => ({
-			name: f,
-			optional: optionals.includes(f),
-		}));
-	return fields;
+  const t = classType.toString();
+  const optionals: string[] = classType.prototype.optionals ?? [];
+  const fields = t.substring(t.indexOf("{") + 1, t.indexOf("constructor"))
+    .split(";").map((s) => s.trim()).filter((s) => s.length > 0).map((f) => ({
+      name: f,
+      optional: optionals.includes(f),
+    }));
+  return fields;
 }
 
 /** ========================================================================
