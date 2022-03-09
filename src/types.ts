5acd186f7967fe54d95bfb772642e7e3d05a7c60
William Rågstad <41281398+WilliamRagstad@users.noreply.github.com>
2022-03-09 21:58:38 +0100

    Update types.ts

--- src/types.ts
+++ src/types.ts
@@ -44,11 +44,61 @@ enum AppMode {
   PROD,
 }
 
-export { AppMode, IController, Request, Response };
+/**
+ * The logging levels of a log message.
+ */
+enum LoggingLevel {
+  Log,
+  Debug,
+  Info,
+  Success,
+  Warning,
+  Error,
+  Critical,
+  Fatal,
+}
+
+/**
+ * Format a string using the standard library `std/fmt/colors.ts` or custom function.
+ */
+type ColorFunction = (s: string) => string;
+
+/**
+ * Formatting functions for the `LoggingLevel` enum.
+ */
+type ColorOptions = {
+  log: ColorFunction;
+  debug: ColorFunction;
+  info: ColorFunction;
+  success: ColorFunction;
+  warn: ColorFunction;
+  error: ColorFunction;
+  critical: ColorFunction;
+  fatal: ColorFunction;
+} & Record<string, ColorFunction>;
+
+/**
+ * A function that formats a log message.
+ */
+type LoggingFormatterData = {
+  message: string;
+  level: LoggingLevel;
+  timestamp: string;
+};
+
+/**
+ * A function that takes a message and returns a formatted string.
+ */
+type LoggingFormatter = (data: LoggingFormatterData) => string;
+
+export { AppMode, IController, LoggingLevel, Request, Response };
 export type {
+  ColorFunction,
+  ColorOptions,
   Constructor,
   Context,
   HTTPMethods,
+  LoggingFormatter,
   Params,
   StringRouterMiddleware,
   Void,
